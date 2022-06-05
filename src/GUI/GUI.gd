dd21a58ad6d55b4508fb93168580d718119f7b7d
Your Name <silver1miner@gmail.com>
2022-06-05 14:54:35 -0700

    GUI bars

---
+++ src/GUI/GUI.gd
@@ -0,0 +1,20 @@
+extends Control
+
+onready var hp_bar = $Status/Bars/HPDisplay/HPBar
+onready var en_bar = $Status/Bars/ENDisplay/ENBar
+onready var level_text = $Status/Bars/Status/Level
+onready var clock = $Status/Bars/Status/Clock
+onready var joystick = $Joypad
+
+func _ready() -> void:
+	pass
+
+func _on_Player_hp_changed(new_hp) -> void:
+	hp_bar.value = new_hp
+
+func _on_Player_en_changed(new_en) -> void:
+	en_bar.value = new_en
+
+func _on_Player_level_changed() -> void:
+	level_text.text = "Level: " + str(PlayerData.current_level)
+	joystick.reset()
