ed09d58923ba6983ca1a781ff5654389ca127700
Your Name <silver1miner@gmail.com>
2022-06-08 19:22:02 -0700

    loading screen

--- src/Game.gd
+++ src/Game.gd
@@ -1,6 +1,48 @@
 extends Node
 
+var loader
+var wait_frames
+var time_max = 100 #msec
+onready var current_scene = $MainMenu
+
 func _ready() -> void:
-	pass
+	$Loading.visible = false
 
+func go_to_scene(path) -> void:
+	loader = ResourceLoader.load_interactive(path)
+	if loader == null:
+		push_error("no scene to load")
+		return
+	set_process(true)
+	current_scene.queue_free()
+	wait_frames = 1
 
+func _process(_delta: float) -> void:
+	if loader == null:
+		$Loading.visible = false
+		set_process(false)
+		return
+	if wait_frames > 0:
+		wait_frames -= 1
+		return
+	var t = OS.get_ticks_msec()
+	while OS.get_ticks_msec() < t + time_max:
+		var err = loader.poll()
+		if err == ERR_FILE_EOF:
+			var resource = loader.get_resource()
+			loader = null
+			set_new_scene(resource)
+			break
+		elif err == OK:
+			update_progress()
+		else:
+			push_error("loading fail")
+			loader = null
+			break
+
+func update_progress() -> void:
+	$Loading.visible = true
+
+func set_new_scene(scene_resource) -> void:
+	current_scene = scene_resource.instance()
+	add_child(current_scene)
--- src/Menus/MainMenu.gd
+++ src/Menus/MainMenu.gd
@@ -1,5 +1,7 @@
 extends Control
 
+onready var Game = get_tree().get_root().get_node("Game")
+
 func _ready() -> void:
 	$SettingsMenu.visible = false
 
@@ -9,3 +11,5 @@ func _on_Start_pressed() -> void:
 func _on_Options_pressed() -> void:
 	$SettingsMenu.visible = true
 
+func _on_Survival_pressed() -> void:
+	Game.go_to_scene("res://src/World/World.tscn")
